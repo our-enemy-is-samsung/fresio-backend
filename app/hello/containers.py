0b3f02343d81f93e1d284b24b031de902d98d5d1
Yuchan Han <serltretu24@gmail.com>
2024-11-21 23:19:19 +0900

    intelij file ignore

--- app/hello/containers.py
+++
@@ -1,9 +0,0 @@
-from dependency_injector import containers, providers
-
-from app.hello.services import HelloService
-from app.hello.repository.message import MessageRepository
-
-
-class HelloContainer(containers.DeclarativeContainer):
-    repository: MessageRepository = providers.Singleton(MessageRepository)
-    service: HelloService = providers.Factory(HelloService, repository=repository)

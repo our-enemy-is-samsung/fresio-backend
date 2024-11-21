9661d99ce1fb2c1de0d41af2f8f16eaae1cbb96b
Yuchan Han <serltretu24@gmail.com>
2024-11-21 23:24:11 +0900

    컨테이너 연결

--- app/containers.py
+++ app/containers.py
@@ -1,7 +1,11 @@
 from dependency_injector import containers, providers
 
-from app.hello.containers import HelloContainer
+from app.auth.containers import AuthContainer
+from app.google.containers import GoogleContainer
+from app.email.containers import EmailContainer
 
 
 class AppContainers(containers.DeclarativeContainer):
-    hello: HelloContainer = providers.Container(HelloContainer)
+    google: "GoogleContainer" = providers.Container(GoogleContainer)
+    auth: "AuthContainer" = providers.Container(AuthContainer, google_service=google)
+    email: "EmailContainer" = providers.Container(EmailContainer)

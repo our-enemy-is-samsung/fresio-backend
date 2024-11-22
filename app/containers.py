7b6b06a38263d8c2e503da1d156c0aa3ed20694e
Yuchan Han <serltretu24@gmail.com>
2024-11-22 12:33:56 +0900

    bracket container 연결

--- app/containers.py
+++ app/containers.py
@@ -1,6 +1,7 @@
 from dependency_injector import containers, providers
 
 from app.auth.containers import AuthContainer
+from app.bracket.containers import BracketContainer
 from app.google.containers import GoogleContainer
 from app.email.containers import EmailContainer
 
@@ -9,3 +10,4 @@ class AppContainers(containers.DeclarativeContainer):
     google: "GoogleContainer" = providers.Container(GoogleContainer)
     auth: "AuthContainer" = providers.Container(AuthContainer, google_service=google)
     email: "EmailContainer" = providers.Container(EmailContainer)
+    bracket: "BracketContainer" = providers.Container(BracketContainer)
